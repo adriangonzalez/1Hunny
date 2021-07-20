bb7deda5e4f6702a290bd087419ebfabeba28e7d
Adrian Gonzalez <adrian.gonzalez@gmail.com>
2021-07-20 12:27:24 -0500

    added code documentation

--- js/site.js
+++ js/site.js
@@ -1,24 +1,50 @@
 // get the values from the page
-//start or controller function
-function getValues(){
+//starts or controller function
+function getValues() {
 	//get values
 	let startValue = document.getElementById("startValue").value;
 	let endValue = document.getElementById("endValue").value;
+	let numbers = [];
 
-	//call generateNumbers
-	alert(startValue + ":" + endValue);
+	//parse into integers
+	startValue = parseInt(startValue);
+	endValue = parseInt(endValue);
+
+	if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+		//call generateNumbers
+		numbers = generateNumbers(startValue, endValue);
+	} else {
+		alert("You must enter integers");
+	}
 
 	//call displayNumbers
+	displayNumbers(numbers);
 }
 
-// generate numbers from starvalue to endvalue
-//logic function(s)
-function generateNumbers(params) {
-	
+// generate numbers from startvalue to endvalue
+//logic functions
+function generateNumbers(sValue, eValue) {
+	let numbers = [];
+
+	for (let index = sValue; index <= eValue; index++) {
+		numbers.push(index);
+	}
+
+	return numbers;
 }
 
 // display the numbers and mark even numbers bold
 //display or view function
-function displayNumbers(params) {
-	
+function displayNumbers(numbers) {
+	let templateRows = "";
+
+	for (let index = 0; index < numbers.length; index++) {
+		let number = numbers[index];
+
+		let className = (number % 2 == 0) ? "even" : "odd";
+
+		templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+	}
+
+	document.getElementById("results").innerHTML = templateRows;
 }
